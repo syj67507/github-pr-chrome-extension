64e7cc3ae382ed885d0cad10592075d95a48c5b4
Shams Jamal <shams.jamal67@gmail.com>
2022-10-12 22:34:40 -0400

    cleaned up organization of saved repo

--- src/popup/components/Options/components/Saved/SavedRepo/index.jsx
+++ src/popup/components/Options/components/Saved/SavedRepo/index.jsx
@@ -6,29 +6,33 @@ import CloseIcon from '@mui/icons-material/Close';
 export default function SavedRepo({ repo, onRemove, bgcolor }) {
   return (
       <Stack
-        direction="column"
+        direction="row"
         padding={1}
-        alignItems="flex-start"
+        alignItems="center"
         width="100%"
         bgcolor={bgcolor}
       >
+
+        {/* Saved information */}
         <Stack
-          direction="row"
-          alignItems="center"
+          direction="column"
+          alignItems="flex-start"
           width="100%"
-          justifyContent="space-between"
         >
           <Typography variant="caption">{repo.url}</Typography>
-          <IconButton
-            aria-label="delete"
-            onClick={onRemove}
-            padding={0}
-          >
+          {repo.jiraDomain && <Typography variant="caption">{repo.jiraDomain}</Typography>}
+          {repo.jiraTag && <Typography variant="caption">{repo.jiraTag}</Typography>}
+        </Stack>
+
+        {/* Delete icon button */}
+        <IconButton
+          aria-label="delete"
+          onClick={onRemove}
+          padding={0}
+        >
           <CloseIcon />
         </IconButton>
-        </Stack>
-        {repo.jiraDomain && <Typography variant="caption">{repo.jiraDomain}</Typography>}
-        {repo.jiraTag && <Typography variant="caption">{repo.jiraTag}</Typography>}
+
     </Stack>
   );
 }
