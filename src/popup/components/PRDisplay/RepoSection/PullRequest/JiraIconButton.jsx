175d56567e64a32d40abb86a9a960688ab512a68
Shams Jamal <shams.jamal67@gmail.com>
2022-10-12 22:08:44 -0400

    changed buttons to icon buttons, added svg support

---
+++ src/popup/components/PRDisplay/RepoSection/PullRequest/JiraIconButton.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import IconButton from "@mui/material/IconButton"
+import Tooltip from "@mui/material/Tooltip"
+import JiraIcon from "./jira-icon.svg";
+import DisabledJiraIcon from "./jira-icon-disabled.svg";
+
+// "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"
+function EnabledJiraIconButton({ jiraUrl }) {
+    return (
+        <Tooltip
+            title={jiraUrl}
+            placement="top"
+        >
+            <IconButton
+                sx={{
+                    border: "2px solid #2684ff",
+                }}
+                onClick={() => {
+                    if (jiraUrl !== undefined) {
+                    chrome.tabs.create({
+                        url: jiraUrl,
+                    });
+                    }
+                }}
+            >
+                <JiraIcon />
+            </IconButton>
+        </Tooltip>
+    )
+}
+
+function DisabledJiraIconButton() {
+    return (
+        <IconButton
+            disabled
+            sx={{
+                border: "2px solid #ddd",
+            }}
+        >
+            <DisabledJiraIcon />
+        </IconButton>
+    )
+}
+
+export default function JiraIconButton({ jiraUrl }) {
+    if (jiraUrl) {
+        return <EnabledJiraIconButton jiraUrl={jiraUrl} />
+    }
+    
+    return <DisabledJiraIconButton />
+}
