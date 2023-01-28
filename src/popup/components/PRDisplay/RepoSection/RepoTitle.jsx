d628880f354d6d23d4717c549d3adfc2802c38ed
syj67507 <shams.jamal67@gmail.com>
2023-01-28 14:00:16 -0500

    Refactor - Switching Back (#15)
    * switched open tab and expand behaviors
    
    * added tooltip for number open
    
    * switching back

--- src/popup/components/PRDisplay/RepoSection/RepoTitle.jsx
+++ src/popup/components/PRDisplay/RepoSection/RepoTitle.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
@@ -8,33 +8,36 @@ import { createTab } from "../../../../data/extension";
 
 export default function RepoTitle({ repo }) {
   return (
-    <Tooltip
-      title={`${repo.pullRequests.length} open`}
-      followCursor
-      disableInteractive
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        padding: 1,
+        borderBottom: "1px solid whitesmoke",
+      }}
     >
       <Box
         sx={{
-          width: "100%",
           display: "flex",
           flexDirection: "row",
-          padding: 1,
-          borderBottom: "1px solid whitesmoke",
+          flex: 1,
+          justifyContent: "center",
+          gap: 1,
+        }}
+        onClick={() => {
+          createTab(repo.url);
         }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            flex: 1,
-            justifyContent: "center",
-            gap: 1,
-          }}
-        >
-          <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
-            {`${repo.owner}/${repo.name}`}
-          </Typography>
-        </Box>
+        <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+          {`${repo.owner}/${repo.name}`}
+        </Typography>
+      </Box>
+      <Tooltip
+        title={`${repo.pullRequests.length} open`}
+        followCursor
+        disableInteractive
+      >
         <Box
           sx={{
             flex: 0,
@@ -46,11 +49,8 @@ export default function RepoTitle({ repo }) {
                 bgcolor: "transparent",
               },
             }}
-            onClick={() => {
-              createTab(repo.url);
-            }}
           >
-            <OpenInNewIcon
+            <ExpandMoreIcon
               sx={{
                 fontSize: 16,
                 color: "rgba(119, 119, 119, 0.25)",
@@ -64,7 +64,7 @@ export default function RepoTitle({ repo }) {
             />
           </IconButton>
         </Box>
-      </Box>
-    </Tooltip>
+      </Tooltip>
+    </Box>
   );
 }
