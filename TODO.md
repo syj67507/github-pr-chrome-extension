b9230e59f4f0fab1d6efedfb9949d6c565bab9c3
Shams Jamal <shams.jamal67@gmail.com>
2023-04-30 11:38:12 -0400

    moved saved and add components into one page

--- TODO.md
+++ TODO.md
@@ -1,11 +1,16 @@
 # TODO List
 
-- Create button components for icons
-- Disabled should have its own styling and no tooltip around it
-- Enabled should have its own styling with a tooltip around it
-
 - Remove the options from webpack config
 - Remove the options from src folder
 - install typescript
 - install prettier and eslint
 - Allow options for hiding certain buttons like (JIRA)
+
+- move the permissions in the folder structure
+- fix the imports to try and reduce the bundle size
+
+- create a privacy policy
+  examples
+  https://crxmouse.com/privacy/
+  https://theorycraft.gg/terms
+  https://betterttv.com/privacy
---
+++ src/popup/components/Options/components/Repos/index.jsx
@@ -0,0 +1,53 @@
+import "regenerator-runtime";
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Add from "./components/Add";
+import Saved from "./components/Saved";
+import {
+  addRepository,
+  getRepositories,
+  getStorage,
+  removeRepository,
+} from "../../../../../data/extension";
+
+export default function Repos() {
+  const [repos, setRepos] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  // Fetch repos on page load
+  useEffect(() => {
+    async function getRepos() {
+      setLoading(true);
+      const storage = await getStorage();
+      setRepos(storage.repos || []);
+      setLoading(false);
+    }
+    getRepos().catch((e) => {
+      console.error(e);
+      setError(true);
+    });
+  }, []);
+
+  return (
+    <Box>
+      <Add
+        onSave={async (repo, jiraTags, jiraDomain) => {
+          await addRepository(repo, jiraTags, jiraDomain);
+          setRepos(await getRepositories());
+          console.log(await getRepositories());
+        }}
+      />
+      <Saved
+        repos={repos}
+        loading={loading}
+        error={error}
+        onRemove={async (repo) => {
+          console.log("onRemove", repo.url);
+          await removeRepository(repo.url);
+          setRepos(await getRepositories());
+        }}
+      />
+    </Box>
+  );
+}
