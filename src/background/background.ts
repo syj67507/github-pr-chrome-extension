6148eed0d11643c2b97b35a70a403967d47c6691
Shams Jamal <shams.jamal67@gmail.com>
2024-02-29 21:41:11 -0500

    Refactor: Sync Storage (#29)

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,12 +1,7 @@
 import Browser from "webextension-polyfill";
 import "regenerator-runtime";
 import GitHubClient from "../data/index";
-import {
-  getToken,
-  clearStorage,
-  getRepositories,
-  setBadge,
-} from "../data/extension";
+import { getToken, getRepositories, setBadge } from "../data/extension";
 
 const alarmName = "fetchPRs";
 const delayInMinutes = 0;
@@ -23,8 +18,6 @@ Browser.runtime.onInstalled.addListener(async () => {
     periodInMinutes,
   });
   console.log("Created!", await Browser.alarms.get(alarmName));
-
-  await clearStorage();
 });
 
 // Periodically fetch pull requests and update the badge
---
+++ src/popup/components/Settings/ClearStorageSetting.tsx
@@ -0,0 +1,28 @@
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import React from "react";
+import * as browser from "../../../data/extension";
+
+export default function ClearStorageSetting() {
+  return (
+    <Stack direction="column" width="100%" padding={2} spacing={2}>
+      <Typography variant="body1" textAlign="center">
+        Any issues may stem from invalid storage data. Clear the storage here.
+      </Typography>
+      <Stack width="100%" direction="row" justifyContent="flex-end" spacing={2}>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => {
+            browser.clearStorage().catch(() => {
+              console.error("Failed to clear storage.");
+            });
+          }}
+        >
+          Clear
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
