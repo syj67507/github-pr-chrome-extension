0ffe6393dfe78f8a1deee0c7202dfa6bf0a2d9d1
Shams Jamal <shams.jamal67@gmail.com>
2024-03-25 09:39:54 -0400

    Feature: Add Visual Feedback on Settings Page (#33)
    - Added Alert component logic to be displayed at the base of the
    settings component
    - Inner settings component now takes in props for the alert state so
    that it can properly send information on what alert to show

---
+++ src/popup/components/Settings/ResetStorageSetting.tsx
@@ -0,0 +1,45 @@
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import React from "react";
+import { type AlertColor } from "@mui/material/Alert";
+import * as browser from "../../../data/extension";
+
+interface ClearStorageSettingProps {
+  setAlertType: React.Dispatch<React.SetStateAction<AlertColor | "none">>;
+  setAlertMessage: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function ResetStorageSetting({
+  setAlertType,
+  setAlertMessage,
+}: ClearStorageSettingProps) {
+  return (
+    <Stack direction="column" width="100%" padding={2} spacing={2}>
+      <Typography variant="body1" textAlign="center">
+        Any issues may stem from invalid storage data. Reset the storage here.
+      </Typography>
+      <Stack width="100%" direction="row" justifyContent="flex-end" spacing={2}>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => {
+            browser
+              .resetStorage()
+              .then(() => {
+                setAlertType("success");
+                setAlertMessage("Storage reset!");
+              })
+              .catch(() => {
+                console.error("Failed to reset storage.");
+                setAlertType("error");
+                setAlertMessage("Failed to reset storage.");
+              });
+          }}
+        >
+          Clear
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
--- src/popup/components/Settings/index.tsx
+++ src/popup/components/Settings/index.tsx
@@ -1,12 +1,35 @@
-import React from "react";
-import ClearStorageSetting from "./ClearStorageSetting";
+import React, { useState } from "react";
+import Alert, { type AlertColor } from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import ResetStorageSetting from "./ResetStorageSetting";
 import AccessTokenSetting from "./AccessTokenSetting";
 
 export default function Settings() {
+  const [alertType, setAlertType] = useState<AlertColor | "none">("none");
+  const [alertMessage, setAlertMessage] = useState("");
+
   return (
-    <>
-      <AccessTokenSetting />
-      <ClearStorageSetting />
-    </>
+    <Stack width="100%">
+      {alertType !== "none" && (
+        <Stack padding={1}>
+          <Alert
+            severity={alertType}
+            onClose={() => {
+              setAlertType("none");
+            }}
+          >
+            {alertMessage}
+          </Alert>
+        </Stack>
+      )}
+      <AccessTokenSetting
+        setAlertType={setAlertType}
+        setAlertMessage={setAlertMessage}
+      />
+      <ResetStorageSetting
+        setAlertType={setAlertType}
+        setAlertMessage={setAlertMessage}
+      />
+    </Stack>
   );
 }
