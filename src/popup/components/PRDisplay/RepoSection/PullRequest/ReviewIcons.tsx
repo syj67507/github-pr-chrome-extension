705f426930e77e75e2d3804845c075b96618ab3f
Shams Jamal <shams.jamal67@gmail.com>
2024-05-16 22:01:06 -0400

    added icons for the users last review state

---
+++ src/popup/components/PRDisplay/RepoSection/PullRequest/ReviewIcons.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import DoneIcon from "@mui/icons-material/Done";
+import FeedbackOutlinedIcon from "@mui/icons-material/FeedbackOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import Tooltip from "@mui/material/Tooltip";
+
+export function ApprovedIcon() {
+  return (
+    <Tooltip
+      title="You have approved this pull request"
+      placement="top"
+      disableInteractive
+    >
+      <DoneIcon
+        sx={{
+          color: "#1f883d",
+        }}
+      />
+    </Tooltip>
+  );
+}
+
+export function ChangesRequestedIcon() {
+  return (
+    <Tooltip
+      title="You have requested changes on this pull request"
+      placement="top"
+      disableInteractive
+    >
+      <FeedbackOutlinedIcon
+        sx={{
+          color: "red",
+        }}
+      />
+    </Tooltip>
+  );
+}
+
+export function CommentedIcon() {
+  return (
+    <Tooltip
+      title="You have left comments on this pull request"
+      placement="top"
+      disableInteractive
+    >
+      <ChatBubbleOutlineOutlinedIcon
+        sx={{
+          color: "#767676",
+        }}
+      />
+    </Tooltip>
+  );
+}
--- src/popup/components/PRDisplay/RepoSection/PullRequest/index.tsx
+++ src/popup/components/PRDisplay/RepoSection/PullRequest/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import DoneIcon from "@mui/icons-material/Done";
+import FeedbackOutlinedIcon from "@mui/icons-material/FeedbackOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import Tooltip from "@mui/material/Tooltip";
 import JiraIconButton from "./JiraIconButton";
 import GitHubIconButton from "./GitHubIconButton";
 import type { PullRequestData } from "../../../../../data";
+import {
+  ApprovedIcon,
+  ChangesRequestedIcon,
+  CommentedIcon,
+} from "./ReviewIcons";
 
 interface PullRequestProps {
   pr: PullRequestData;
@@ -23,7 +32,7 @@ export default function PullRequest({
       direction="row"
       alignItems="center"
       spacing={1}
-      width="100%"
+      flex={1}
       sx={{
         "&:hover": { bgcolor: "whitesmoke" },
       }}
@@ -32,7 +41,7 @@ export default function PullRequest({
     >
       <GitHubIconButton pr={pr} />
       {isJiraConfigured && <JiraIconButton jiraUrl={pr.jiraUrl} />}
-      <Stack overflow="hidden">
+      <Stack overflow="hidden" flex={1}>
         <Typography variant="caption" fontStyle="italic">
           {pr.username}
         </Typography>
@@ -48,6 +57,21 @@ export default function PullRequest({
           {pr.title}
         </Typography>
       </Stack>
+
+      {/* Icons to track if the user reviewed the pull request  */}
+      {pr.viewerLatestReview !== null && (
+        <Stack
+          sx={{
+            justifySelf: "flex-end",
+          }}
+        >
+          {pr.viewerLatestReview === "APPROVED" && <ApprovedIcon />}
+          {pr.viewerLatestReview === "CHANGES_REQUESTED" && (
+            <ChangesRequestedIcon />
+          )}
+          {pr.viewerLatestReview === "COMMENTED" && <CommentedIcon />}
+        </Stack>
+      )}
     </Stack>
   );
 }
